ad10407f33a68b097e31801da25ee873f3565f74
Abhishek Singh <abhisheksingh12208799@gmail.com>
2024-10-13 23:03:02 +0530

    learn linear search

---
+++ Array/linearSearch.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class linearSearch{
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 34;
+        arr[3] = 40;
+        for(int i=0;i<4;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
